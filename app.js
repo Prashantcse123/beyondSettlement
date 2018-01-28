91ad1bb9b35c5065eb37e03634955444a003806a
Jose Osorio <josorio2210@gmail.com>
2018-01-28 17:25:44 -0500

    Add api versioning example

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const routes = require('./routes/index');
-const users = require('./routes/users');
+const routes = require('./routes/api/v1/index');
+const users = require('./routes/api/v1/users');
 
 const app = express();
 
@@ -30,8 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('api/v1/', routes);
+app.use('api/v1/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
