addfc30b010c3efc2ae410caaf1a78a5d07653d8
Uri Michaeli <urixwd@gmail.com>
2018-07-08 23:47:03 +0200

    remove force:true

--- dbSync.js
+++ dbSync.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 console.log({ message: `Seeding ${process.env.NODE_ENV} environment, port ${port}` });
 debug({ message: `sSeeding ${process.env.NODE_ENV} environment, port ${port}` });
 
-models.sequelize.sync({ force: true }).then(() => {
+models.sequelize.sync().then(() => {
   /**
    * Listen on provided port, on all network interfaces.
    */
