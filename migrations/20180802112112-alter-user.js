e02c1627423c3539953a3117f3fbb058b9d622ea
sumitstpl <sumitsyst@gmail.com>
2018-08-02 23:19:37 +0530

    added code for migration

---
+++ migrations/20180802112112-alter-user.js
@@ -0,0 +1,25 @@
+/* An example of migration in Sequelize */
+
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
