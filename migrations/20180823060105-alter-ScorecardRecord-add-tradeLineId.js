98306aefd531c9711a260844e964cdd37a567a24
sumitstpl <sumitsyst@gmail.com>
2018-08-24 02:49:40 +0530

    more migraions

--- migrations/20180823060105-alter-ScorecardRecord-add-tradeLineId.js
+++ migrations/20180823060105-alter-ScorecardRecord-add-tradeLineId.js
@@ -2,13 +2,13 @@
 var models = require('../models')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // return queryInterface.addColumn('ScorecardRecords', 'tradeLineId', {
-    //   type: Sequelize.INTEGER,
-    //   /* allowNull: false,
-    //   unique: true,
-    //   primaryKey: true, */
-    //   after: 'tradeLineName',
-    // })
+    return queryInterface.addColumn('ScorecardRecords', 'tradeLineId', {
+      type: Sequelize.INTEGER,
+      /* allowNull: false,
+      unique: true,
+      primaryKey: true, */
+      after: 'tradeLineName',
+    })
   },
 
   down: (queryInterface, Sequelize) => { }
--- migrations/20180823060115-alter-ScorecardRecord-change-tradeLineId.js
+++ migrations/20180823060115-alter-ScorecardRecord-change-tradeLineId.js
@@ -2,6 +2,7 @@
 var models = require('../models')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    /* TODO change tradeline id columns to make relations */
     // await queryInterface.removeColumn('ScorecardRecords', 'isDone');
   },
 
