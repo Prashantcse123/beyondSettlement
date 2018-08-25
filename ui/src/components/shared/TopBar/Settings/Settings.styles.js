0c9d41ab45392cac0aea52d03c9f3a90b7487aff
Uri Michaeli <urixwd@gmail.com>
2018-08-25 17:55:40 +0200

    Remove ui folder (moved to beyond-settlements-client)

--- ui/src/components/shared/TopBar/Settings/Settings.styles.js
+++
@@ -1,9 +0,0 @@
-import GlobalStyles from '../../../Global.styles';
-
-export default function styles(theme) {
-    return Object.assign({}, GlobalStyles(theme), {
-        settings: {
-
-        }
-    });
-};
--- ui/src/stores/stores.js
+++
@@ -1,9 +0,0 @@
-import AppState from "./AppState";
-import ScorecardStore from "./ScorecardStore";
-import SystemProgressStore from "./SystemProgressStore";
-
-export default {
-	appState: new AppState(),
-    scorecard: new ScorecardStore(),
-    systemProgress: new SystemProgressStore(),
-};
